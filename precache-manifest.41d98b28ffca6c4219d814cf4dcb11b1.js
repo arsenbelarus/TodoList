efb9d32296b6500ee4677647e9988eb5fdb65aa9
arsenbelarus <arsenbelarus@gmail.com>
2020-08-05 22:30:36 +0300

    Updates

---
+++ precache-manifest.41d98b28ffca6c4219d814cf4dcb11b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db23b1cf512da92858014d2c12227e6",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "0756beca3b9c1d33e21a",
+    "url": "/TodoList/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "5b99af3df71135f8fd45",
+    "url": "/TodoList/static/js/2.029802fe.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/TodoList/static/js/2.029802fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0756beca3b9c1d33e21a",
+    "url": "/TodoList/static/js/main.3052006f.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
